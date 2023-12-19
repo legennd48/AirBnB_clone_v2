d27118f37735ee931ec3279ede9eaf52b8537e93
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-19 22:07:47 +0000

    initial commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
 
 
 env = getenv('HBNB_TYPE_STORAGE')

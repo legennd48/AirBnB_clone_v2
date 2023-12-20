e2fb287ff57ebfc177667cd91da2d49d9132d710
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-20 10:54:17 +0000

    fixed issues with selecting btw dg and fs

--- models/amenity.py
+++ models/amenity.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from os import getenv
 
 
 class Amenity(BaseModel, Base):
@@ -11,6 +12,9 @@ class Amenity(BaseModel, Base):
     amenity clss for amenities
     '''
     __tablename__ = 'amenities'
-    name = Column(String(60), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
-                                   back_populates="amenities")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(60), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ''
--- models/place.py
+++ models/place.py
@@ -17,25 +17,36 @@ place_amenity = Table('place_amenity', Base.metadata,
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    amenity_ids = []
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024))
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
+        amenity_ids = []
         reviews = relationship("Review", backref="place",
                                cascade='all, delete, delete-orphan')
         amenities = relationship('Amenity',
                                  secondary=place_amenity, viewonly=False,
                                  back_populates='place_amenities')
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             '''

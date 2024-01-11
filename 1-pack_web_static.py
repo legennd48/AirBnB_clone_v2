f13ba0b714995e8dfc56ad3d42aec86957ff6401
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-11 12:12:20 +0000

    requirments fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,11 +6,15 @@ Fabric script that generates a .tgz from the content of
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
+    """create .tgz archive of web_static/ folder"""
     try:
-        arch_name = "web_static_{}.tgz".format(datetime.now().strftime('%Y%m%d%H%M%S'))
+        arch_name = "web_static_{}.tgz".format(
+            datetime.now().strftime('%Y%m%d%H%M%S'))
         local("mkdir -p ./versions/")
-        local("tar -cvzf ./versions/{} -C {} .".format(arch_name, "./web_static"))
+        local("tar -cvzf ./versions/{} -C {} .".format(
+            arch_name, "./web_static"))
         return "./versions/{}".format(arch_name)
     except Exception as e:
         return None

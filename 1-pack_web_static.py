3b6b12de0c631af7c6a23a892aeb5684813a5746
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-13 15:58:56 +0000

    small changes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,9 +12,9 @@ def do_pack():
     try:
         arch_name = "web_static_{}.tgz".format(
             datetime.now().strftime('%Y%m%d%H%M%S'))
-        local("mkdir -p versions/")
-        local("tar -cvzf versions/{} -C {} .".format(
-            arch_name, "web_static"))
+        local("mkdir -p versions")
+        local("tar -cvzf versions/{} {}".format(
+            arch_name, "web_static/"))
         return "versions/{}".format(arch_name)
     except Exception as e:
         return None

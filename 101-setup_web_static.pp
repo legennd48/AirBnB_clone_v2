038779e4d14bc0aa89b3896a21eb1bc7c408d0a7
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-14 12:07:52 +0000

    new puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,8 +1,34 @@
 # Puppet manifest to set up a web server
 
+# Nginx configuration file
+$conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+
 # Installing Nginx
 package { 'nginx':
-  ensure => installed,
+  ensure   => 'present',
+  provider => 'apt'
 }
 
 # Create folders
@@ -16,7 +42,7 @@ file { '/data/web_static/shared/':
 
 # Create fake index.html
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
+  ensure  => 'present',
   content => 'this is just a fake HTML for testing',
 }
 
@@ -27,23 +53,17 @@ file { '/data/web_static/current':
 }
 
 # Give ownership of /data/ to ubuntu
-file { '/data/':
-  ensure  => directory,
-  recurse => true,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 # Configure Nginx server to serve the content of /data/web_static/current/ to hbnb_static
-file_line { 'nginx_hbnb_static_config':
-  path    => '/etc/nginx/sites-enabled/default',
-  line    => '    location /hbnb_static/ {',
-  before  => '    server_name _;',
-  content => "        alias /data/web_static/current/;\n    }\n",
-}
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $conf
+} ->
 
 # Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  subscribe => File['/etc/nginx/sites-enabled/default'],
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
